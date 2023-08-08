681e2a7ceca14375e006a686112e7eb00380503b
Caio <caiomendesfaria@gmail.com>
2023-08-08 12:30:28 -0300

    first commit

---
+++ exerc01.py
@@ -0,0 +1,12 @@
+nome = "Caio"
+sobrenome = "Mendes"
+idade = 19
+peso = "55kg"
+altura = 1.70
+maior = "maior de idade"
+
+print(nome,sobrenome)
+print(idade,"anos")
+print(peso)
+print(altura)
+print(maior)
