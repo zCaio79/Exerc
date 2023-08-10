1feff76c32221710506a0b559a7a797e102c7235
Caio <caiomendesfaria@gmail.com>
2023-08-10 17:14:21 -0300

    alpha

--- exerc01.py
+++ exerc01.py
@@ -3,10 +3,12 @@ sobrenome = "Mendes"
 idade = 19
 peso = "55kg"
 altura = 1.70
-maior = "maior de idade"
 
 print(nome,sobrenome)
 print(idade,"anos")
 print(peso)
 print(altura)
-print(maior)
+if idade>=18:
+    print("Maior De idade")
+else:
+    print("Menor De idade")
