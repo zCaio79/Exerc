a76b696f40cc05cf223479721cd7b1769c6b75b0
Caio <caiomendesfaria@gmail.com>
2023-08-26 15:14:45 -0300

    alpha

--- exerc06.py
+++ exerc06.py
@@ -1,24 +1,22 @@
 import os
 import time
 #Média das Notas /by Caio
-nome = [2]
-nota = [2]
-cont = 0
+
+alunos = {}
+i = 0
 soma = 0
-while (cont <= 2):
-    os.system('cls')
-    auxiliarnome = input("Digite o nome do aluno: ")
-    nome.append(auxiliarnome)
-    auxiliarnota = float(input("Digite a nota do aluno: "))
-    nota.append(auxiliarnota)
-    os.system('cls')
-    print("Aluno ",auxiliarnome," Cadastrado!! | Nota : ",auxiliarnota)
-    cont = (cont+1)
-    time.sleep(3)
-cont = 0
-while (cont <= 2):
-    soma = soma + nota[cont]
-    cont = (cont+1)
-soma = soma/3
 os.system('cls')
-print("Média das Notas = ",soma)
+while i<=2:
+    auxnome = str(input("Digite o Nome do Aluno: "))
+    auxnota = float(input("Insira a Nota do Aluno: "))
+    alunos[auxnome] = auxnota
+    i = i+1
+    os.system('cls')
+    print("Aluno Adicionado")
+    time.sleep(2)
+    os.system('cls')
+for x in alunos:
+    print(x," : ",alunos[x])
+    soma = soma+ alunos[x]
+soma = soma/3
+print("Média das Notas : ",soma)
